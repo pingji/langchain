97dae899eb5892e7b585046fbfac9f5529ba4d32
吉平 <jiping@Ping-Mac-mini.local>
2024-06-05 00:36:00 +0800

    update lesson 01

--- 01_LangChain快速入门/03_TextLangChain.py
+++ 01_LangChain快速入门/03_TextLangChain.py
@@ -2,12 +2,16 @@
 https://time.geekbang.org/column/intro/100617601
 作者 黄佳'''
 
-import os
-os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
+from dotenv import load_dotenv  # 用于加载环境变量
+load_dotenv()  # 加载 .env 文件中的环境变量
+
+# import os
+# os.environ["OPENAI_API_KEY"] = '你的OpenAI API Key'
+
 from langchain.llms import OpenAI
 llm = OpenAI(  
-    model="text-davinci-003",
+    model="davinci-002",
     temperature=0.8,
-    max_tokens=60,)
+    max_tokens=60)
 response = llm.predict("请给我的花店起个名")
 print(response)
